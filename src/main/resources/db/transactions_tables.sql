ec4b2479c73b80cd8cd4285479b32b06855aa7fd
mrglaster <pristavkaegor03@gmail.com>
2023-07-25 17:24:01 +0300

    Namings changes

--- src/main/resources/db/transactions_tables.sql
+++ src/main/resources/db/transactions_tables.sql
@@ -1,17 +1,26 @@
+
 -- Table describing the operation category
 CREATE TABLE IF NOT EXISTS operation_category(
     id BIGSERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL UNIQUE,
     cashback_percent DOUBLE PRECISION NOT NULL,
-    status INTEGER NOT NULL
+    status INTEGER NOT NULL,
+    ts BIGINT
 );
 
+CREATE SEQUENCE IF NOT EXISTS operation_category_seq
+    MINVALUE 1
+    MAXVALUE 999999999
+    INCREMENT BY 1
+    START WITH 1;
+
 -- Table describing organization
-CREATE TABLE IF NOT EXISTS organisation(
+CREATE TABLE IF NOT EXISTS organization(
     id BIGSERIAL PRIMARY KEY,
     code UUID NOT NULL UNIQUE,
     cashback_percent DOUBLE PRECISION NOT NULL,
-    status INTEGER NOT NULL
+    status INTEGER NOT NULL,
+    ts BIGINT
 );
 
 -- Table describing transaction
@@ -22,8 +31,13 @@ CREATE TABLE IF NOT EXISTS transactions(
     balance_before_transaction DOUBLE PRECISION NOT NULL,
     client_id BIGSERIAL NOT NULL,
     date TIMESTAMP(6) WITHOUT TIME ZONE NOT NULL,
-    organisation_code UUID NOT NULL ,
+    organization_code UUID NOT NULL ,
     operation_category VARCHAR(255) NOT NULL,
-    FOREIGN KEY (organisation_code) REFERENCES organisation(code),
+    ts BIGINT,
+    FOREIGN KEY (organization_code) REFERENCES organization(code),
     FOREIGN KEY (operation_category) REFERENCES operation_category(name)
 );
+
+DELETE FROM operation_category;
+DELETE FROM organization;
+DELETE FROM transactions;
