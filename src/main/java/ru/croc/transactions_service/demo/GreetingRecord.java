8d867bd878e3482d647a2006596295c6ce1ab0b2
mrglaster <pristavkaegor03@gmail.com>
2023-07-25 10:41:44 +0300

    initial commit

---
+++ src/main/java/ru/croc/transactions_service/demo/GreetingRecord.java
@@ -0,0 +1,4 @@
+package ru.croc.transactions_service.demo;
+
+public record GreetingRecord(long status, String content) {
+}
---
+++ src/test/java/ru/croc/transactions_service/TransactionsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.croc.transactions_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
